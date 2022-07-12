701881eb27c59977c13e75be78d3e0cfe225bd20
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-07-12 04:05:31 +0100

    my go

--- settingUp/arrays.go
+++ settingUp/arrays.go
@@ -1,1 +1,18 @@
 package main
+
+import "fmt"
+
+func main() {
+	myArr()
+}
+
+func myArr() {
+	var arr [5]int
+	for i := 0; i < len(arr); i++ {
+		println("enter a number: ")
+		var number = arr[i]
+		fmt.Scanf("%d", &number)
+
+		fmt.Print(number, " ")
+	}
+}
